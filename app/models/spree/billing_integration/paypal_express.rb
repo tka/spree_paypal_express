298cd1ad90cfb5462b95273e6478e10e13c324f7
tka <tkatka@gmail.com>
2011-11-18 10:05:36 +0800

    paypal password & signature should not blank

--- app/models/spree/billing_integration/paypal_express.rb
+++ app/models/spree/billing_integration/paypal_express.rb
@@ -1,11 +1,13 @@
 class Spree::BillingIntegration::PaypalExpress < Spree::BillingIntegration
   preference :login, :string
   preference :password, :password
-  preference :signature, :string
+  preference :signature, :password
   preference :review, :boolean, :default => false
   preference :no_shipping, :boolean, :default => false
   preference :currency, :string, :default => 'USD'
 
+  validates :preferred_password, :presence => true
+  validates :preferred_signature, :presence => true
   def provider_class
     ActiveMerchant::Billing::PaypalExpressGateway
   end
